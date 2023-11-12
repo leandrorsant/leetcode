8f4f8abf19e5add8ebe834b744a5e6341c8bfa33
Leandro <ramosdossantos.leandro@gmail.com>
2023-11-12 06:55:19 -0300

    RotateArray solved

---
+++ RotateArray.js
@@ -0,0 +1,59 @@
+/*
+189. Rotate Array
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+
+ 
+
+Constraints:
+
+    1 <= nums.length <= 105
+    -231 <= nums[i] <= 231 - 1
+    0 <= k <= 105
+
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+        let startIndex = (Math.abs(nums.length - (k)) % nums.length)
+
+        //console.log(startIndex)
+        let row = []
+        for(let y=0;y<nums.length;y++){
+            row.push( nums[ (startIndex + y) % nums.length ] )
+        }
+        console.log(row);
+        for(let y=0;y<nums.length;y++){
+            nums[y] = row[y]
+        }
+}
+
+let nums = [1,2,3]
+let k = 4
+
+console.log(JSON.stringify(nums))
+for(let x=1;x<=k;x++){
+rotate(nums,k)
+console.log(JSON.stringify(nums)+" k: "+x)
+}
+
